7b310499aae8da4c19184865db4bc4b063956b4e
Fatima Arab <w5463256@macmini11.gbccloud.com>
2025-02-08 11:14:40 -0500

    Initial commit for lab1

---
+++ Lab1_Fatima_arab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab1_Fatima_arab
+//
+//  Created by Fatemeh (Fatima) Arab on 2025-02-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab1_Fatima_arab/Lab1_Fatima_arabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Fatima_arabApp.swift
+//  Lab1_Fatima_arab
+//
+//  Created by Fatemeh (Fatima) Arab on 2025-02-08.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Fatima_arabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
